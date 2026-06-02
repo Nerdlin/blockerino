26cc07d0a977f68f32fdf2200d84b2bcbd7d0917
Nerdlin <nerdlinnut@gmail.com>
2026-06-02 21:07:53 +0500

    feat: обновить конфигурацию Babel и добавить новые плагины

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,11 @@ module.exports = function(api) {
       ['babel-preset-expo', { jsxRuntime: 'automatic', unstable_transformImportMeta: true }]
     ],
     plugins: [
-      'babel-plugin-transform-import-meta'
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+      'babel-plugin-transform-import-meta',
+      ['babel-plugin-react-compiler', { ReactCompiler: true }]
     ]
   };
 };
