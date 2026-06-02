14dc8c1042486cd927600c5869b60d58a67692f4
Nerdlin <nerdlinnut@gmail.com>
2026-06-02 20:21:59 +0500

    feat: добавить плагин babel-plugin-transform-import-meta и обновить конфигурацию Babel

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,10 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      ['babel-preset-expo', { unstable_transformImportMeta: true }]
+      ['babel-preset-expo', { jsxRuntime: 'automatic', unstable_transformImportMeta: true }]
     ],
+    plugins: [
+      'babel-plugin-transform-import-meta'
+    ]
   };
 };
