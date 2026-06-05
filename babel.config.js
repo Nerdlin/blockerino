f6ae069d4052feb1615a91dd192470a8a3019d0f
Nerdlin <nerdlinnut@gmail.com>
2026-06-05 17:56:53 +0500

    fix: Удалить параметр loose из плагинов Babel для улучшения совместимости

--- babel.config.js
+++ babel.config.js
@@ -5,9 +5,9 @@ module.exports = function(api) {
       ['babel-preset-expo', { jsxRuntime: 'automatic', unstable_transformImportMeta: true }]
     ],
     plugins: [
-      ['@babel/plugin-transform-class-properties', { loose: true }],
-      ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+      '@babel/plugin-transform-class-properties',
+      '@babel/plugin-transform-private-methods',
+      '@babel/plugin-transform-private-property-in-object',
       'babel-plugin-transform-import-meta',
       ['babel-plugin-react-compiler', { ReactCompiler: true }]
     ]
