0b4d9eeb1026597092515e7ba5c7bbd147304765
Nerdlin <nerdlinnut@gmail.com>
2026-07-22 15:55:19 +0500

    chore: restore custom domain blockerino.aionerdlin.me

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default ({ config }) => ({
   ...config,
   experiments: {
     ...config.experiments,
-    baseUrl: process.env.GH_PAGES ? "/blockerino" : undefined
+    baseUrl: undefined
   },
   plugins: [
     ...(config.plugins || []),
